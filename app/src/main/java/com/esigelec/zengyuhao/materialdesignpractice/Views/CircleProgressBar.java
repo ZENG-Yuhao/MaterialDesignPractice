59af6fe4b8dc7ef11d0a18aa14dfb4429ab8ae86
ENZO <enzo.zyh@gmail.com>
2016-04-23 01:09:43 +0200

    Custom View

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/Views/CircleProgressBar.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/Views/CircleProgressBar.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.esigelec.zengyuhao.materialdesignpractice.R;
+
 /**
  * Created by enzoz on 2016/4/22.
  */
@@ -13,6 +15,7 @@ public class CircleProgressBar extends View {
     private int mBarWidth;
     private Color mBarColor;
     private BarStyle mBarStyle;
+    private int mPercentage;
 
     private enum BarStyle {
         STATIC,
@@ -25,9 +28,19 @@ public class CircleProgressBar extends View {
 
     public CircleProgressBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CircleProgressBar, 0, 0);
+
+        try {
+            mBarWidth = a.getInteger(R.styleable.CircleProgressBar_barWidth, 0);
+            mPercentage = a.getInteger(R.styleable.CircleProgressBar_percentage, 0);
+        } finally {
+            a.recycle();
+        }
     }
 
     public CircleProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
+
 }
