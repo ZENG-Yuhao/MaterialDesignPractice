8adde8ccad0e3aa3784226f930b3db7b14e7161d
ZENG Yuhao <enzo.zyh@gmail.com>
2016-05-16 11:55:47 +0200

    Some modifications

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/SharedElementsTransitionActivity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/SharedElementsTransitionActivity.java
@@ -9,33 +9,42 @@ import android.widget.ImageView;
 
 /**
  * Setting exit transition (xml)
- *  1.add an explode-transition into resources folder @see ./res/transition/grid_exit.xml
- *  2.declare the explode-transition in AppTheme @see ./res/values/styles.xml
- *  3.create bundle using ActivityOptions.makeSceneTransitionAnimation(context) (different with shared-element transi.)
+ * 1.add an explode-transition into resources folder @see ./res/transition/grid_exit.xml
+ * 2.declare the explode-transition in AppTheme @see ./res/values/styles.xml
+ * 3.create bundle using ActivityOptions.makeSceneTransitionAnimation(context) (different with shared-element transi.)
+ * <p/>
  *
  * Setting enter transition (code)
- *  1.add Slide programmatically @see #SharedElementDetailsActivity
- *  2.add target for Slide instance
- *  3.config (not compulsive)
- *  4.getWindow().setEnterTransition()
+ * 1.add Slide programmatically @see #SharedElementDetailsActivity
+ * 2.add target for Slide instance
+ * 3.config (not compulsive)
+ * 4.getWindow().setEnterTransition()
+ * <p/>
  *
  * Setting shared-elements transition
- *  1.Indicate same android:transitionName between shared elements
- *          (transitionName is a string for that it can be used between applications)
- *  2.Create Bundle using ActivityOptions.makeSceneTransitionAnimation(context, (sharedView, sharedViewName)...)
- *  3.Start the activity with bundle
+ * 1.Indicate same android:transitionName between shared elements
+ * (transitionName is a string for that it can be used between applications)
+ * 2.Create Bundle using ActivityOptions.makeSceneTransitionAnimation(context, (sharedView, sharedViewName)...)
+ * 3.Start the activity with bundle
+ * <p/>
  *
  * ### Attention ###
- *  1.We may face the problem that shared-elements transitions does not work. Make sure the bundle is created off the
- *    onCreate() method. Because in onCreate(), the view does not have height and width yet. So the
- *    makeSceneTransitionAnimation() method may create the wrong animations.
- *  2.We may need declare windowContentTransition to true in style.xml. This attribute will be set to true automatically
- *    if the AppTheme inherits already AppCompat or Material. @see ./res/values/styles.xml
+ * 1.We may face the problem that shared-elements transitions does not work. Make sure the bundle is created off the
+ * onCreate() method. Because in onCreate(), the view does not have height and width yet. So the
+ * makeSceneTransitionAnimation() method may create the wrong animations.
+ * <p/>
  *
+ * 2.We may see <b>blinking effect</b> on status bar or navigation bar when transition between two activities.
+ * That's because these bars fade in and out during the transition.
+ * More details: {@see
+ * <a href="http://stackoverflow.com/questions/26600263/how-do-i-prevent-the-status-bar-and-navigation-bar-from-animating-during-an-acti">Fix Blinking Effect</a> }
+ *
+ * 3.We may need declare windowContentTransition to true in style.xml. This attribute will be set to true automatically
+ * if the AppTheme inherits already AppCompat or Material. @see ./res/values/styles.xml
+ * <p/>
  * Reference:
- * @see <a href="http://developer.android.com/training/material/animations.html">Defining Custom Animations</a>
  * @sse <a href="https://classroom.udacity.com/courses/ud862/lessons/4969789009/concepts/49083289230923">Udacity</a>
- *
+ * @see <a href="http://developer.android.com/training/material/animations.html">Defining Custom Animations</a>
  */
 public class SharedElementsTransitionActivity extends Activity {
 
