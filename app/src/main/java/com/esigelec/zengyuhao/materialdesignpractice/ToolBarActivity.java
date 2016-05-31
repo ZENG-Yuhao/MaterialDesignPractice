b85083c85f09856f05cb838dac3cd8b7fc8d73fc
ZENG Yuhao <enzo.zyh@gmail.com>
2016-05-31 22:33:13 +0200

    Some modifications

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/ToolBarActivity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/ToolBarActivity.java
@@ -3,6 +3,7 @@ package com.esigelec.zengyuhao.materialdesignpractice;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class ToolBarActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class ToolBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tool_bar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 }
