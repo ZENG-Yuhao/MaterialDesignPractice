81b0c05bf774b02560d8bba9106c7e0d594bbadb
ZENG Yuhao <enzo.zyh@gmail.com>
2016-05-31 22:58:20 +0200

    ToolBar

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/ToolBarActivity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/ToolBarActivity.java
@@ -2,8 +2,10 @@ package com.esigelec.zengyuhao.materialdesignpractice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.media.MediaBrowserCompatUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class ToolBarActivity extends AppCompatActivity {
 
@@ -14,5 +16,11 @@ public class ToolBarActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
     }
 }
