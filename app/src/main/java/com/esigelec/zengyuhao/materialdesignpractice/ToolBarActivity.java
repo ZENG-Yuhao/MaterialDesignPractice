35df5f3edd83f77a145ccf0e9ef84578ccc45bf9
ZENG Yuhao <enzo.zyh@gmail.com>
2016-05-31 23:06:13 +0200

    ToolBar

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/ToolBarActivity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/ToolBarActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.media.MediaBrowserCompatUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ToolBarActivity extends AppCompatActivity {
 
@@ -15,12 +17,27 @@ public class ToolBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tool_bar);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setTitle("ToolBar");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_items, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_attach:
+                Toast.makeText(ToolBarActivity.this, "Item attach selected.", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(ToolBarActivity.this, "Item settings selected.", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
