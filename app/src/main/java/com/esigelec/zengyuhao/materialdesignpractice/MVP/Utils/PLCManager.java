b2f62dd447cc52c94f37d9bdc8c91f58277ff502
ZENG Yuhao <enzo.zyh@gmail.com>
2016-06-08 22:47:56 +0200

    MVP

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/MVP/Utils/PLCManager.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/MVP/Utils/PLCManager.java
@@ -4,6 +4,7 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Loader;
 import android.os.Bundle;
+import android.os.Debug;
 
 /**
  * Presenter Loader Callbacks Manager.
@@ -17,18 +18,22 @@ public class PLCManager<P extends IPresenter, V extends IView> implements Loader
     private InstanceProvider<P> mInstanceProvider;
 
     /**
-     *
      * @param context used to retrieve application context.
-     * @param view used to set and reset the presenter that is bound to current view/activity.
+     * @param view    used to set and reset the presenter that is bound to current view/activity.
      * @param factory used to offer a factory to create an instance of specific presenter in loader.
      */
     public PLCManager(Context context, V view, IPresenterFactory<P> factory) {
         mContext = context;
 
         /*
-         * Here, in PLCManager, we hold a strong reference of the activity, and the instance of PLCManager is
-         * referenced in the activity, this makes a circular reference. System GC can handle this situation, so there
-          * is no need to detach the view(activity) and it will not cause a memory leak.
+         * Here, in PLCManager, we hold a strong reference of the Activity, and the instance of PLCManager is
+         * referenced in the Activity, also PLCManager has implemented LoaderCallbacks interface, which will be
+         * passed to and will be saved in LoaderManager when initLoader() or restartLoader(), just,
+         * LoaderManager has the same lifecycle with Activity, their relationship is like below:
+         *        Activity <--> PLCManager <-- LoaderManager
+         *
+         * All these make a circular reference. But system GC can handle this situation, so there is no need to
+         * detach the view(activity) and it will not cause a memory leak.
          */
         mView = view;
         mFactory = factory;
@@ -40,18 +45,20 @@ public class PLCManager<P extends IPresenter, V extends IView> implements Loader
     }
 
     @Override
-    public void onLoadFinished(Loader<P> loader, P presenter){
+    public void onLoadFinished(Loader<P> loader, P presenter) {
         mView.setPresenter(presenter);
     }
 
     @Override
-    public void onLoaderReset(Loader<P> loader){
-        mView.setPresenter(null);
+    public void onLoaderReset(Loader<P> loader) {
+        if (mView != null)
+            mView.setPresenter(null);
     }
 
-    public void setInstanceProvider(InstanceProvider<P> provider){
+    public void setInstanceProvider(InstanceProvider<P> provider) {
         mInstanceProvider = provider;
     }
+
     public interface InstanceProvider<P> {
         P provide();
     }
