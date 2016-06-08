182a7fb7d4d8af0eacb52ec6752dce1da061eb5a
ZENG Yuhao <enzo.zyh@gmail.com>
2016-06-08 22:11:45 +0200

    MVP

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/MVP/View/Login3Activity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/MVP/View/Login3Activity.java
@@ -117,7 +117,7 @@ public class Login3Activity<P extends ILoginPresenter> extends Activity implemen
          * detach the view for the presenter here, because the presenter is hold in Loader who has a much longer
          * lifecycle than activity, and our view(this activity) is attached to the presenter, that means if we
          * don't do presenter.onViewDetach() here, when view(activity) is stopped to the backend, and when GC try to
-         * recycler this activity, it can't, since the activity is still referenced by the presenter.
+         * recycler this activity, it can't, since the activity is still referenced by the presenter (in Loader).
          */
         mPresenter.onViewDetach();
         super.onStop();
