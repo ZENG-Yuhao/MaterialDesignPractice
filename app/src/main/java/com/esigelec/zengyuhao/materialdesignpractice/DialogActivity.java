0f60553da2052bd918368df73d01f8ad866a8238
ZENG Yuhao <enzo.zyh@gmail.com>
2016-06-12 19:06:38 +0200

    Dialog

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/DialogActivity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/DialogActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class DialogActivity extends Activity {
@@ -60,9 +63,25 @@ public class DialogActivity extends Activity {
     }
 
     public static class CustomViewDialogFragment extends DialogFragment {
+        private OnActionsListener mOnActionsListener;
+        protected EditText editxt_account, editxt_pswd;
+        protected Button btn_login, btn_clear;
+
+//        @Override
+//        public void onAttach(Context context) {
+//            super.onAttach(context);
+//            try {
+//                mOnActionsListener = (OnActionsListener) context;
+//            } catch (ClassCastException e){
+//                throw new ClassCastException(context.toString()+" does not implement OnActionsListener.");
+//            }
+//        }
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            View view = getActivity().getLayoutInflater().inflate(R.layout.activity_login, null);
+
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setTitle("Normal AlertDialog")
                     .setMessage("This is a normal AlertDialog")
@@ -78,8 +97,24 @@ public class DialogActivity extends Activity {
                             Toast.makeText(getActivity(), "Cancel", Toast.LENGTH_SHORT).show();
                         }
                     })
-                    .setView(R.layout.activity_login);
+                    .setView(view);
+
+
+            editxt_account = (EditText) view.findViewById(R.id.editxt_account);
+            editxt_pswd = (EditText) view.findViewById(R.id.editxt_pswd);
+            btn_login = (Button) view.findViewById(R.id.btn_login);
+            btn_login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), "login clicked", Toast.LENGTH_SHORT).show();
+                }
+            });
             return builder.create();
+        }
+
+        public interface OnActionsListener {
+            void onLogin(String account, String password);
+            void onDissmiss();
         }
     }
 }
