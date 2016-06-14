abd5ac09582890c869630fac61788274d9fd16f8
root <enzo.zyh@gmail.com>
2016-06-14 16:34:57 +0200

    BottomToolBar

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/CustomizedViews/BottomToolBar/BottomToolBarRefused.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/CustomizedViews/BottomToolBar/BottomToolBarRefused.java
@@ -1,10 +1,14 @@
 package com.esigelec.zengyuhao.materialdesignpractice.CustomizedViews.BottomToolBar;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ActionMenuView;
@@ -14,26 +18,27 @@ import android.widget.LinearLayout;
  * A bottom tool bar that can receive a list of tabs and their appropriate actions.
  * Created by root on 13/06/16.
  */
-public class BottomToolBar extends LinearLayout {
+public class BottomToolBarRefused extends LinearLayout {
     private Adapter adapter;
     private ViewHolder[] list_holder;
     private int itemCount;
     private OnItemClickListener onItemClickListener;
     private int currentPosition = 0;
+    Animator anim, anim1;
 
-    public BottomToolBar(Context context) {
+    public BottomToolBarRefused(Context context) {
         super(context);
     }
 
-    public BottomToolBar(Context context, AttributeSet attrs) {
+    public BottomToolBarRefused(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public BottomToolBar(Context context, AttributeSet attrs, int defStyleAttr) {
+    public BottomToolBarRefused(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    public BottomToolBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public BottomToolBarRefused(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
@@ -72,12 +77,21 @@ public class BottomToolBar extends LinearLayout {
                 if height and width get 0 before the first load of the layout, register listener to obtain height and
                  width when onLayout() finished.
              */
-            ViewTreeObserver observer = this.getViewTreeObserver();
+            final ViewTreeObserver observer = this.getViewTreeObserver();
             if (observer != null) {
                 observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                     @Override
                     public void onGlobalLayout() {
                         initTabs(getHeight(), getWidth());
+
+                        ViewTreeObserver observerLocal = getViewTreeObserver();
+                        if (null != observerLocal) {
+                            // removeOnGlobalLayoutListener() is only supported by SDK later than JELLY_BEAN
+                            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN)
+                                observerLocal.removeGlobalOnLayoutListener(this);
+                            else
+                                observerLocal.removeOnGlobalLayoutListener(this);
+                        }
                     }
                 });
             }
@@ -99,6 +113,7 @@ public class BottomToolBar extends LinearLayout {
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, height);
             params.gravity = Gravity.CENTER;
             view.setLayoutParams(params);
+            Log.i("Haha", "initTabs--->>" + "i " + i + "; itemCount" + itemCount + ";" + view.getParent());
             this.addView(view);
         }
     }
@@ -117,7 +132,8 @@ public class BottomToolBar extends LinearLayout {
         holder.itemView.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (currentPosition == position) return;
+                tempAnim(v);
+                //if (currentPosition == position) return;
 
                 animateTransition(position);
                 // call user customized method
@@ -129,6 +145,68 @@ public class BottomToolBar extends LinearLayout {
         });
     }
 
+    private void tempAnim(View v){
+        // calculate params
+        double half_width = v.getWidth() / 2;
+        double half_height = v.getHeight() / 2;
+        int finalRadius = (int) Math.hypot(half_width, half_height);
+
+        // set exit animator
+        anim = ViewAnimationUtils.createCircularReveal(v, (int) half_width, (int) half_height,
+                finalRadius, 0);
+        anim.setDuration(200);
+        anim.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                anim1.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        // set enter animator
+        anim1 = ViewAnimationUtils.createCircularReveal(v, (int) half_width, (int) half_height, 0,
+                finalRadius);
+        anim1.setDuration(200);
+        anim1.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // animation set finished, reset the clickable state
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        // start exit animator and then enter animator
+        anim.start();
+    }
+
     private void animateTransition(int selectedPos) {
         // display animation
         if (list_holder[selectedPos].onFocusAnimSet != null)
@@ -138,11 +216,11 @@ public class BottomToolBar extends LinearLayout {
             list_holder[currentPosition].onLostFocusAnimSet.start();
     }
 
-    public abstract class ViewHolder {
+    public static abstract class ViewHolder {
         public View itemView;
-        protected int position = 0;
-        protected AnimatorSet onFocusAnimSet;
-        protected AnimatorSet onLostFocusAnimSet;
+        public int position = 0;
+        public AnimatorSet onFocusAnimSet;
+        public AnimatorSet onLostFocusAnimSet;
 
         public ViewHolder(View view) {
             if (view == null) {
@@ -161,7 +239,7 @@ public class BottomToolBar extends LinearLayout {
     }
 
 
-    public abstract class Adapter<VH extends ViewHolder> {
+    public static abstract class Adapter<VH extends ViewHolder> {
 
         protected final VH createViewHolder(ViewGroup parent) {
             VH holder = onCreateViewHolder(parent);
--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/EXEM/BottomToolBarActivity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/EXEM/BottomToolBarActivity.java
@@ -1,15 +1,70 @@
 package com.esigelec.zengyuhao.materialdesignpractice.EXEM;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.esigelec.zengyuhao.materialdesignpractice.CustomizedViews.BottomToolBar.BottomToolBarRefused;
 import com.esigelec.zengyuhao.materialdesignpractice.R;
 
 public class BottomToolBarActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_tool_bar);
+
+        BottomToolBarRefused toolBar = (BottomToolBarRefused) findViewById(R.id.bottom_toolbar);
+        toolBar.setAdapter(new MyAdapter());
+        toolBar.setOnItemClickListener(new BottomToolBarRefused.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+            }
+        });
+    }
+
+    public class MyAdapter extends BottomToolBarRefused.Adapter<MyAdapter.MyHolder> {
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
+
+        @Override
+        public MyHolder onCreateViewHolder(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_bottom_tool_bar, parent, false);
+
+            TextView txt = (TextView) view.findViewById(R.id.tab_title);
+            ImageView img = (ImageView) view.findViewById(R.id.tab_icon);
+            MyHolder holder = new MyHolder(view, img, txt);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyHolder holder, int position) {
+            holder.img.setImageResource(R.drawable.ic_devices_black_48dp);
+            holder.txt.setText("TAB" + String.valueOf(position));
+            holder.view.setElevation(position * 4);
+        }
+
+        public class MyHolder extends BottomToolBarRefused.ViewHolder {
+            public View view;
+            public ImageView img;
+            public TextView txt;
+
+            public MyHolder(View view, ImageView img, TextView txt) {
+                super(view);
+                this.view = view;
+                this.img = img;
+                this.txt = txt;
+            }
+        }
+
     }
 }
