5066d32d5a1d44da891a16011e94ef9783904bed
root <enzo.zyh@gmail.com>
2016-06-23 17:25:28 +0200

    SwipeLayout

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/CustomizedViews/SwipeLayout/SwipeLayout.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/CustomizedViews/SwipeLayout/SwipeLayout.java
@@ -9,7 +9,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 
