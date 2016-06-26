ad2d719520974e1859c73772715101429fb90360
ZENG Yuhao <enzo.zyh@gmail.com>
2016-06-26 18:24:55 +0200

    ..

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/SwipeLayoutActivity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/SwipeLayoutActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.esigelec.zengyuhao.materialdesignpractice.CustomizedViews.SwipeLayout.SwipeLayout;
@@ -16,19 +17,19 @@ public class SwipeLayoutActivity extends Activity {
         setContentView(R.layout.activity_swipe_layout);
 
         SwipeLayout swipeLayout = (SwipeLayout) findViewById(R.id.swipe_layout);
-        Button btn1 = (Button) swipeLayout.findViewById(R.id.btn_1);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        ImageView btn_edit = (ImageView) swipeLayout.findViewById(R.id.btn_edit);
+        btn_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SwipeLayoutActivity.this, "Edit", Toast.LENGTH_SHORT).show();
             }
         });
 
-        Button btn2 = (Button) swipeLayout.findViewById(R.id.btn_2);
-        btn2.setOnClickListener(new View.OnClickListener() {
+        ImageView btn_delete = (ImageView) findViewById(R.id.btn_delete);
+        btn_delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hello Android", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SwipeLayoutActivity.this, "Delete", Toast.LENGTH_SHORT).show();
             }
         });
     }
