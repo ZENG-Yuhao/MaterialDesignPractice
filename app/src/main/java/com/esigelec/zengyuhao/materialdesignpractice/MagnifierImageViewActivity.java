30dbfa3f065e8f66375c138b4afd3c857ff81fc8
root <enzo.zyh@gmail.com>
2016-07-04 17:03:12 +0200

    ..

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/MagnifierImageViewActivity.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/MagnifierImageViewActivity.java
@@ -1,15 +1,45 @@
 package com.esigelec.zengyuhao.materialdesignpractice;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
+import android.renderscript.Float2;
+import android.util.EventLogTags;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
+import com.esigelec.zengyuhao.materialdesignpractice.CustomViews.MagnifierImageView;
+
 public class MagnifierImageViewActivity extends Activity {
+    private ImageView img;
+    private MagnifierImageView.Magnifier magnifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magnifier_image_view);
-        ImageView img;
+
+        magnifier = (MagnifierImageView.Magnifier) findViewById(R.id.magnifier);
+
+        img = (ImageView) findViewById(R.id.img1);
+        img.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_MOVE:
+                    case MotionEvent.ACTION_UP:
+                        float x = event.getX();
+                        float y = event.getY();
+                        float relativeX = x / img.getWidth();
+                        float relativeY = y / img.getHeight();
+                        magnifier.updateCenterByRelativeVals(relativeX, relativeY);
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 }
