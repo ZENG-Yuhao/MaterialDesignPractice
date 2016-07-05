09ce05161a0d84873e9655ce1bacdc7903813c63
root <enzo.zyh@gmail.com>
2016-07-05 17:31:38 +0200

    ..

---
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/Core/Helper/GPSLocatorHelper.java
@@ -0,0 +1,15 @@
+package com.esigelec.zengyuhao.materialdesignpractice.Core.Helper;
+
+/**
+ * <p>
+ * Created by ZENG Yuhao on 05/07/16. <br>
+ * Contact: enzo.zyh@gmail.com
+ * </p>
+ */
+public class GPSLocatorHelper {
+
+    public class Locator {
+        public float pivotX, pivotY;
+
+    }
+}
