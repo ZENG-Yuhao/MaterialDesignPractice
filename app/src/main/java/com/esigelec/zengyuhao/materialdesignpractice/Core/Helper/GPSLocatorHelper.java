0a301d1d85357a7efa4919537145acd1c058fc21
root <enzo.zyh@gmail.com>
2016-07-05 18:26:21 +0200

    ..

--- app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/Core/Helper/GPSLocatorHelper.java
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/Core/Helper/GPSLocatorHelper.java
@@ -1,5 +1,11 @@
 package com.esigelec.zengyuhao.materialdesignpractice.Core.Helper;
 
+import android.graphics.Bitmap;
+import android.view.View;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * <p>
  * Created by ZENG Yuhao on 05/07/16. <br>
@@ -7,9 +13,60 @@ package com.esigelec.zengyuhao.materialdesignpractice.Core.Helper;
  * </p>
  */
 public class GPSLocatorHelper {
+    private Bitmap mBitmap;
 
-    public class Locator {
+    public GPSLocatorHelper(View attachedView, View... locatorViews) {
+        this(attachedView, null, locatorViews);
+    }
+
+    public GPSLocatorHelper(View attachedView, Bitmap bitmap, View... locatorViews) {
+        mBitmap = bitmap;
+        init(attachedView, convertToLocators(locatorViews));
+    }
+
+    public GPSLocatorHelper(View attachedView, ArrayList<View> locatorViewList) {
+        this(attachedView, null, locatorViewList);
+    }
+
+    public GPSLocatorHelper(View attachedView, Bitmap bitmap, ArrayList<View> locatorViewList) {
+        mBitmap = bitmap;
+        init(attachedView, convertToLocators(locatorViewList));
+    }
+
+    public GPSLocatorHelper(View attachedView, Locator... locators) {
+        this(attachedView, null, locators);
+    }
+
+
+    public GPSLocatorHelper(View attachedView, Bitmap bitmap, Locator... locators) {
+        mBitmap = bitmap;
+        init(attachedView, convertToLocators(locators));
+    }
+
+    /**
+     * Root Initialization
+     *
+     * @param attachedView attached view.
+     * @param locatorList  locator list.
+     */
+    public void init(View attachedView, ArrayList<Locator> locatorList) {
+
+    }
+
+    public ArrayList<Locator> convertToLocators(View[] views) {
+        return null;
+    }
+
+    public ArrayList<Locator> convertToLocators(ArrayList<View> views) {
+        return null;
+    }
+
+    public ArrayList<Locator> convertToLocators(Locator[] locators) {
+        return null;
+    }
+
+    public static class Locator {
         public float pivotX, pivotY;
-
+        public View locatorView;
     }
 }
