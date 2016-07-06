8f2fc789aeabfe3034e1f7758687f85d5ff9403f
root <enzo.zyh@gmail.com>
2016-07-06 18:20:00 +0200

    ..

---
+++ app/src/main/java/com/esigelec/zengyuhao/materialdesignpractice/Core/TempClass.java
@@ -0,0 +1,16 @@
+package com.esigelec.zengyuhao.materialdesignpractice.Core;
+
+import com.esigelec.zengyuhao.materialdesignpractice.Core.Helper.GPSLocatorHelper;
+
+/**
+ * <p>
+ * Created by ZENG Yuhao on 06/07/16. <br>
+ * Contact: enzo.zyh@gmail.com
+ * </p>
+ */
+public class TempClass extends GPSLocatorHelper.ActionsCoordinator {
+
+    public TempClass(GPSLocatorHelper helper) {
+        super(helper);
+    }
+}
